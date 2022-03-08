9aacbbf8cd07656c09689a18297f5100ea6f595b
Maksymenko Ivan <69015616+MaksymenkoIvan@users.noreply.github.com>
2022-03-08 21:07:39 +0100

    Delete brawl_stars_last directory

--- brawl_stars_last/src/main/java/org/example/Config.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Config {
-    protected String TABLE_NAME= "users";
-    protected String HOST = "jdbc:mysql://localhost:3306/testDB?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-    protected String USER = "test_user";
-    protected String PASS = "test123";
-}
